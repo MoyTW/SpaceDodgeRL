36e801292a00b99233f5abd5d94f7d09999a8a6c
Travis Moy <moytravis@gmail.com>
2020-10-12 19:00:42 -0700

    Atlases are hard, apparently
    rip atlases
    atlai? heh

--- scenes/HelloWorld.cs
+++ scenes/HelloWorld.cs
@@ -6,6 +6,7 @@ public class HelloWorld : Node2D {
     // We're gonna build our entity on the fly here
     PackedScene entityPrefab = GD.Load<PackedScene>("res://scenes/entities/Entity.tscn");
     PackedScene positionComponentPrefab = GD.Load<PackedScene>("res://scenes/components/PositionComponent.tscn");
+    AtlasTexture atlas = GD.Load<AtlasTexture>("res://resources/atlas_@.tres");
 
     GD.Print("Hello World!");
 
@@ -15,6 +16,20 @@ public class HelloWorld : Node2D {
 
     var positionComponent = positionComponentPrefab.Instance() as PositionComponent;
     positionComponent.Init(new Position(3, 5));
+    newEntity.AddChild(positionComponent);
+
+    this.AddChild(newEntity);
+
+    // Create an enemy
+    newEntity = entityPrefab.Instance() as Entity;
+    newEntity.Init("uuid#2", "the enemy scout");
+
+    positionComponent = positionComponentPrefab.Instance() as PositionComponent;
+    positionComponent.Init(new Position(5, 5));
+    // This is real jankity!
+    atlas.Region = new Rect2(new Vector2(3 * 24, 7 * 36), new Vector2(24, 36));
+    positionComponent.Texture = atlas;
+
     newEntity.AddChild(positionComponent);
 
     this.AddChild(newEntity);
