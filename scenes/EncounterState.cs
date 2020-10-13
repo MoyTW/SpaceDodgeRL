2781fcac8ebd37572600559c17a6f378ae7ffd3b
Travis Moy <moytravis@gmail.com>
2020-10-13 16:28:39 -0700

    Break atlas into distinct textures
    whew that's much less silly

--- scenes/EncounterState.cs
+++ scenes/EncounterState.cs
@@ -5,25 +5,23 @@ public class EncounterState : Node {
 
   public InputHandler inputHandlerRef = null;
 
-  // TODO: Move this out of EncounterState
   private PackedScene _entityPrefab = GD.Load<PackedScene>("res://scenes/entities/Entity.tscn");
+  // Component prefabs
   private PackedScene _positionComponentPrefab = GD.Load<PackedScene>("res://scenes/components/PositionComponent.tscn");
   private PackedScene _playerComponentPrefab = GD.Load<PackedScene>("res://scenes/components/PlayerComponent.tscn");
   private PackedScene _testAIComponentPrefab = GD.Load<PackedScene>("res://scenes/components/AI/TestAIComponent.tscn");
+
   // TODO: Bake the sub-textures into their own resources and load them instead of doing this Very Silly process
-  private string _atlasPath = "res://resources/atlas_@.tres";
-  private Rect2 _AtSignRect2 = new Rect2(new Vector2(0, 4 * 36), new Vector2(24, 36));
+  private string _sPath = "res://resources/atlas_s.tres";
+  private string _AtSignPath = "res://resources/atlas_@.tres";
 
   // TODO: Move this out of EncounterState, create a better way of selecting the sprite than "hey an arbitrary rect"
-  private Entity CreateEntity(string id, string name, GamePosition pos, Rect2 atlasRegion) {
+  private Entity CreateEntity(string id, string name, GamePosition pos, string texturePath) {
     Entity newEntity = _entityPrefab.Instance() as Entity;
     newEntity.Init(id, name);
 
     var positionComponent = _positionComponentPrefab.Instance() as PositionComponent;
-    AtlasTexture atlas = new AtlasTexture();
-    atlas.Atlas = GD.Load<AtlasTexture>(_atlasPath);
-    atlas.Region = atlasRegion;
-    positionComponent.Init(pos, atlas);
+    positionComponent.Init(pos, GD.Load<Texture>(texturePath));
 
     newEntity.AddChild(positionComponent);
 
@@ -32,14 +30,14 @@ public class EncounterState : Node {
 
   public override void _Ready() {
     // ===== PLAYER HACK CREATION
-    var player = this.CreateEntity("uuid#1", "player", new GamePosition(3, 5), _AtSignRect2);
+    var player = this.CreateEntity("uuid#1", "player", new GamePosition(3, 5), _AtSignPath);
     // TODO: Entity & Component hooks into add/remove, so we can use groupings properly!
     player.AddChild(this._playerComponentPrefab.Instance());
     player.AddToGroup("player");
     this.AddChild(player);
 
     // ===== AI SCOUT HACK CREATION
-    var scout = this.CreateEntity("uuid#2", "scout", new GamePosition(5, 5), new Rect2(new Vector2(3 * 24, 7 * 36), new Vector2(24, 36)));
+    var scout = this.CreateEntity("uuid#2", "scout", new GamePosition(5, 5), _sPath);
     scout.AddChild(this._testAIComponentPrefab.Instance());
     this.AddChild(scout);
 
