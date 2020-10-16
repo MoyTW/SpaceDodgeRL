1a5533fcf111ed6e30e63e033189369a67aee701
Travis Moy <moytravis@gmail.com>
2020-10-16 14:12:41 -0700

    Pathfinding & scout moves
    There's...some issue with this though I get an exception every time it
    hits the move code because it can't find the OptimizedPriorityQueue
    import. Which I know it can *compile* in, but somehow Godot's not
    actually linking it at runtime...?

---
+++ library/encounter/Pathfinder.cs
@@ -0,0 +1,59 @@
+using Priority_Queue;
+using System.Collections.Generic;
+using SpaceDodgeRL.scenes.encounter;
+using System.Linq;
+
+namespace SpaceDodgeRL.library.encounter {
+
+  public class Pathfinder {
+
+    public List<EncounterPosition> AStar(EncounterPosition start, EncounterPosition end, EncounterState state) {
+      return AStarWithNewGrid(start, end, state);
+    }
+
+    // TODO: If this ends up being slow, we can mark with a dirty bit
+    /**
+     * Returns a path from start to end, exclusive - that is, the start and end nodes are *not* listed in the path. Does a full
+     * naive search every time.
+     */
+    public static List<EncounterPosition> AStarWithNewGrid(EncounterPosition start, EncounterPosition end, EncounterState state) {
+      SimplePriorityQueue<EncounterPosition> frontier = new SimplePriorityQueue<EncounterPosition>();
+      frontier.Enqueue(start, 0f);
+
+      var cameFrom = new Dictionary<EncounterPosition, EncounterPosition>();
+
+      var costSoFar = new Dictionary<EncounterPosition, float>();
+      costSoFar[start] = 0f;
+
+      while (frontier.Count > 0) {
+        var currentPosition = frontier.Dequeue();
+        var adjacentPositions = state.AdjacentPositions(currentPosition);
+
+        if (adjacentPositions.Contains(currentPosition)) {
+          var path = new List<EncounterPosition>() { end, currentPosition };
+
+          EncounterPosition cameFromPos;
+          while (cameFrom.TryGetValue(path[path.Count - 1], out cameFromPos) && (cameFromPos != start)) {
+            path.Add(cameFromPos);
+          }
+          path.Reverse();
+
+          return path;
+        }
+
+        var adjacentUnblocked = adjacentPositions.Where(adjacent => !state.IsPositionBlocked(adjacent)).ToList();
+        adjacentUnblocked.ForEach(adjacent => {
+          var newNextPositionCost = costSoFar[currentPosition] + 1f;
+          if (!costSoFar.ContainsKey(adjacent) || newNextPositionCost < costSoFar[adjacent]) {
+            costSoFar[adjacent] = newNextPositionCost;
+            // Uses straight-line distance as heuristic
+            float priority = newNextPositionCost + adjacent.DistanceTo(end);
+            frontier.Enqueue(adjacent, priority);
+            cameFrom[adjacent] = currentPosition;
+          }
+        });
+      }
+      return null;
+    }
+  }
+}
--- scenes/components/AI/ScoutAIComponent.cs
+++ scenes/components/AI/ScoutAIComponent.cs
@@ -31,6 +31,14 @@ namespace SpaceDodgeRL.scenes.components.AI {
       var parentPos = parent.GetComponent<PositionComponent>().EncounterPosition;
       var playerPos = state.Player.GetComponent<PositionComponent>().EncounterPosition;
 
+      // Close distance if further than 5
+      if (parentPos.DistanceTo(playerPos) >= 5f) {
+        // TODO: We may want to make pathfinding stateful/cache it or something, to save on turn times
+        var path = Pathfinder.AStarWithNewGrid(parentPos, playerPos, state);
+        if (path != null) {
+          actions.Add(new MoveAction(parent.EntityId, path[0]));
+        }
+      }
       // Always fire
       var fire = new FireProjectileAction(
         parent.EntityId,
