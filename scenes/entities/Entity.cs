3fa39255d1ad91489ff274735d6e0483c3fd43ce
Travis Moy <moytravis@gmail.com>
2020-11-09 14:45:27 -0800

    Optimize component fetching
    Well it cut the time spent in GetComponent() down to 1/3 of its previous
    sum which is again another, like, 25% improvement over the previous time
    but it's still very slow!!!

--- scenes/entities/Entity.cs
+++ scenes/entities/Entity.cs
@@ -1,6 +1,7 @@
 using Godot;
 using SpaceDodgeRL.scenes.components;
 using System;
+using System.Collections.Generic;
 
 namespace SpaceDodgeRL.scenes.entities {
 
@@ -12,9 +13,20 @@ namespace SpaceDodgeRL.scenes.entities {
     public string EntityId { get => _id; }
     public string EntityName { get => _name; }
 
+    // Currently we make the assumption that an Entity can have 1 and only 1 of each node of any particular inheritance tree. So,
+    // a node can only have 1 AIComponent, for example. This might change if we model, I don't know, a status effect as a node -
+    // but if you can have 4 status effects on you and a caller is asking for "the" status effect (via StatusEffect class) that's
+    // logically nonsensical. So, the assumption at least now isn't that terrible.
+    //
+    // I will admit that the implementation is imperfect.
+    private Dictionary<Type, Node> _childTypeToComponent;
+    private Dictionary<Node, List<Type>> _childComponentToTypes;
+
     public void Init(string id, string name) {
       _id = id;
       _name = name;
+      _childTypeToComponent = new Dictionary<Type, Node>();
+      _childComponentToTypes = new Dictionary<Node, List<Type>>();
       AddToGroup(ENTITY_GROUP);
     }
 
@@ -29,11 +41,15 @@ namespace SpaceDodgeRL.scenes.entities {
         throw new NotImplementedException();
       }
       base.AddChild(node, legibleUniqueName);
+      _childTypeToComponent[node.GetType()] = node;
+      _childComponentToTypes[node] = new List<Type>() { node.GetType() };
       AddToGroup((node as Component).EntityGroup);
     }
 
     public new void RemoveChild(Node node) {
       base.RemoveChild(node);
+      _childComponentToTypes[node].ForEach(t => _childTypeToComponent.Remove(t));
+      _childComponentToTypes.Remove(node);
       RemoveFromGroup((node as Component).EntityGroup);
     }
 
@@ -41,10 +57,25 @@ namespace SpaceDodgeRL.scenes.entities {
       throw new InvalidOperationException("Use GetComponent<T>() instead!");
     }
 
-    // TODO: It may be a problem if I end up having Too Many components in each Entity and this slows down.
+    /**
+     * Returns the component matching the type. If there are multiple components matching the type, returns an arbitrary
+     * matching component - though having multiple components match the type you're calling means you're trying to fetch an
+     * inappropriate interface! Also it probably means you want a container component.
+     */
     public T GetComponent<T>() where T: Node {
+      Node foundNode;
+      _childTypeToComponent.TryGetValue(typeof(T), out foundNode);
+      if (foundNode != null) {
+        return foundNode as T;
+      }
+
       foreach (Node child in this.GetChildren()) {
-        if (child is T) { return child as T; }
+        if (child is T) {
+          var typeT = typeof(T);
+          this._childTypeToComponent[typeT] = child as T;
+          this._childComponentToTypes[child].Add(typeT);
+          return child as T;
+        }
       }
       return default(T);
     }
