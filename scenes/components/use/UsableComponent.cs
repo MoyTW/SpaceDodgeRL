f20b8376e95f51b41228501963058390c8571c8a
MoyTW <moytravis@gmail.com>
2020-11-23 13:40:27 -0800

    save/load UsableComponent

--- scenes/components/use/UsableComponent.cs
+++ scenes/components/use/UsableComponent.cs
@@ -1,8 +1,10 @@
+using System.Text.Json;
 using Godot;
+using SpaceDodgeRL.scenes.entities;
 
 namespace SpaceDodgeRL.scenes.components.use {
 
-  public class UsableComponent: Component {
+  public class UsableComponent : Component, Savable {
     public static readonly string ENTITY_GROUP = "USABLE_COMPONENT_GROUP";
     public string EntityGroup => ENTITY_GROUP;
 
@@ -10,5 +12,17 @@ namespace SpaceDodgeRL.scenes.components.use {
       var component = new UsableComponent();
       return component;
     }
+
+    public static UsableComponent Create(string saveData) {
+      return UsableComponent.Create();
+    }
+
+    public string Save() {
+      return JsonSerializer.Serialize(this);
+    }
+
+    public void NotifyAttached(Entity parent) { }
+
+    public void NotifyDetached(Entity parent) { }
   }
 }
