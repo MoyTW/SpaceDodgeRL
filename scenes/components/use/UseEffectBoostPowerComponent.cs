068bf04b9f126347b825e3a4166701669aa14edb
MoyTW <moytravis@gmail.com>
2020-11-23 14:33:20 -0800

    UseEffectBoostPower

--- scenes/components/use/UseEffectBoostPowerComponent.cs
+++ scenes/components/use/UseEffectBoostPowerComponent.cs
@@ -1,12 +1,16 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SpaceDodgeRL.scenes.entities;
+
 namespace SpaceDodgeRL.scenes.components.use {
 
-  public class UseEffectBoostPowerComponent: Component {
+  public class UseEffectBoostPowerComponent: Component, Savable {
     public static readonly string ENTITY_GROUP = "USE_EFFECT_BOOST_POWER_COMPONENT_GROUP";
     public string EntityGroup => ENTITY_GROUP;
 
     // Rename "power" (as in, attack power) to something that's less generic
-    public int BoostPower { get; private set; }
-    public int Duration { get; private set; }
+    [JsonInclude] public int BoostPower { get; private set; }
+    [JsonInclude] public int Duration { get; private set; }
 
     public static UseEffectBoostPowerComponent Create(int boostPower, int duration) {
       var component = new UseEffectBoostPowerComponent();
@@ -16,5 +20,17 @@ namespace SpaceDodgeRL.scenes.components.use {
 
       return component;
     }
+
+    public static UseEffectBoostPowerComponent Create(string saveData) {
+      return JsonSerializer.Deserialize<UseEffectBoostPowerComponent>(saveData);
+    }
+
+    public string Save() {
+      return JsonSerializer.Serialize(this);
+    }
+
+    public void NotifyAttached(Entity parent) { }
+
+    public void NotifyDetached(Entity parent) { }
   }
 }
