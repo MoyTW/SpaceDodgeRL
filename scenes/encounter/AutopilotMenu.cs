4e2d456d12cb355f8a9fe2447714553010ebaa00
MoyTW <moytravis@gmail.com>
2020-12-10 10:34:28 -0800

    Namespace Autopilot, Inventory menus

--- scenes/encounter/AutopilotMenu.cs
+++ scenes/encounter/AutopilotMenu.cs
@@ -1,98 +1,98 @@
 using System.Collections.ObjectModel;
 using Godot;
-using SpaceDodgeRL.scenes;
 using SpaceDodgeRL.scenes.components;
 using SpaceDodgeRL.scenes.encounter.state;
-using SpaceDodgeRL.scenes.entities;
 
-public class AutopilotMenu : HBoxContainer {
-  private static PackedScene _readoutPrefab = GD.Load<PackedScene>("res://scenes/encounter/AutopilotZoneReadout.tscn");
-  private static string ZONE_BUTTON_GROUP = "ZONE_BUTTON_GROUP";
+namespace SpaceDodgeRL.scenes.encounter {
+  public class AutopilotMenu : HBoxContainer {
+    private static PackedScene _readoutPrefab = GD.Load<PackedScene>("res://scenes/encounter/AutopilotZoneReadout.tscn");
+    private static string ZONE_BUTTON_GROUP = "ZONE_BUTTON_GROUP";
 
-  private Button _closeButton;
+    private Button _closeButton;
 
-  public override void _Ready() {
-    _closeButton = this.GetNode<Button>("SidebarContainer/ButtonConsole/CloseButton");
-    _closeButton.Connect("pressed", this, nameof(OnButtonPressed), new Godot.Collections.Array() { null });
-    _closeButton.GrabFocus();
-    _closeButton.Connect("tree_entered", this, nameof(OnTreeEntered));
-  }
-
-  private void ResetZones(ReadOnlyCollection<EncounterZone> zones, int mapWidth, int mapHeight, bool hasIntel) {
-    var systemMap = this.GetNode<Control>("SystemMap");
-    var sidebarButtons = this.GetNode<VBoxContainer>("SidebarContainer/ButtonConsole/DynamicButtonsContainer");
-
-    // Clear all the old zone data
-    foreach(Node child in systemMap.GetChildren()) {
-      systemMap.RemoveChild(child);
-      child.QueueFree();
-    }
-    foreach (Node child in sidebarButtons.GetChildren()) {
-      sidebarButtons.RemoveChild(child);
-      child.QueueFree();
+    public override void _Ready() {
+      _closeButton = GetNode<Button>("SidebarContainer/ButtonConsole/CloseButton");
+      _closeButton.Connect("pressed", this, nameof(OnButtonPressed), new Godot.Collections.Array() { null });
+      _closeButton.GrabFocus();
+      _closeButton.Connect("tree_entered", this, nameof(OnTreeEntered));
     }
 
-    foreach (EncounterZone zone in zones) {
-      // Add the system
-      var systemButton = new Button();
-      systemButton.Text = zone.ZoneName;
-      systemButton.AddToGroup(AutopilotMenu.ZONE_BUTTON_GROUP);
-      systemButton.Connect("pressed", this, nameof(OnButtonPressed), new Godot.Collections.Array() { zone.ZoneId });
-      // TODO: It doesn't scale if you resize the window
-      // TODO: Make it look less dumb
-      float x1Percentage = (zone.X1 + 1) / (float)mapWidth;
-      float y1Percentage = (zone.Y1 + 1) / (float)mapHeight;
-      float scaledX1 = systemMap.RectSize.x * x1Percentage;
-      float scaledY1 = systemMap.RectSize.y * y1Percentage;
+    private void ResetZones(ReadOnlyCollection<EncounterZone> zones, int mapWidth, int mapHeight, bool hasIntel) {
+      var systemMap = GetNode<Control>("SystemMap");
+      var sidebarButtons = GetNode<VBoxContainer>("SidebarContainer/ButtonConsole/DynamicButtonsContainer");
 
-      systemButton.RectPosition = new Vector2(scaledX1, scaledY1);
+      // Clear all the old zone data
+      foreach (Node child in systemMap.GetChildren()) {
+        systemMap.RemoveChild(child);
+        child.QueueFree();
+      }
+      foreach (Node child in sidebarButtons.GetChildren()) {
+        sidebarButtons.RemoveChild(child);
+        child.QueueFree();
+      }
 
-      float widthPercentage = (zone.Width) / (float)mapWidth;
-      float heightPercentage = (zone.Height) / (float)mapHeight;
-      float scaledWidth = systemMap.RectSize.x * widthPercentage;
-      float scaledHeight = systemMap.RectSize.y * heightPercentage;
+      foreach (EncounterZone zone in zones) {
+        // Add the system
+        var systemButton = new Button();
+        systemButton.Text = zone.ZoneName;
+        systemButton.AddToGroup(ZONE_BUTTON_GROUP);
+        systemButton.Connect("pressed", this, nameof(OnButtonPressed), new Godot.Collections.Array() { zone.ZoneId });
+        // TODO: It doesn't scale if you resize the window
+        // TODO: Make it look less dumb
+        float x1Percentage = (zone.X1 + 1) / (float)mapWidth;
+        float y1Percentage = (zone.Y1 + 1) / (float)mapHeight;
+        float scaledX1 = systemMap.RectSize.x * x1Percentage;
+        float scaledY1 = systemMap.RectSize.y * y1Percentage;
 
-      systemButton.RectSize = new Vector2(scaledWidth, scaledHeight);
+        systemButton.RectPosition = new Vector2(scaledX1, scaledY1);
 
-      systemMap.AddChild(systemButton);
+        float widthPercentage = zone.Width / (float)mapWidth;
+        float heightPercentage = zone.Height / (float)mapHeight;
+        float scaledWidth = systemMap.RectSize.x * widthPercentage;
+        float scaledHeight = systemMap.RectSize.y * heightPercentage;
 
-      // Add the sidebar
-      var sidebarReadout = _readoutPrefab.Instance() as AutopilotZoneReadout;
-      sidebarReadout.SetReadout(zone, hasIntel);
-      sidebarReadout.AutopilotButton.Connect("pressed", this, nameof(OnButtonPressed), new Godot.Collections.Array() { zone.ZoneId });
-      sidebarButtons.AddChild(sidebarReadout);
+        systemButton.RectSize = new Vector2(scaledWidth, scaledHeight);
+
+        systemMap.AddChild(systemButton);
+
+        // Add the sidebar
+        var sidebarReadout = _readoutPrefab.Instance() as AutopilotZoneReadout;
+        sidebarReadout.SetReadout(zone, hasIntel);
+        sidebarReadout.AutopilotButton.Connect("pressed", this, nameof(OnButtonPressed), new Godot.Collections.Array() { zone.ZoneId });
+        sidebarButtons.AddChild(sidebarReadout);
+      }
     }
-  }
 
-  public void PrepMenu(EncounterState state) {
-    bool hasIntel = state.Player.GetComponent<PlayerComponent>().KnowsIntel(state.DungeonLevel);
-    ResetZones(state.Zones, state.MapWidth, state.MapHeight, hasIntel);
-    
-    // TODO: Add You Are Here onto the starmap too!
-    // You Are Here label
-    var playerPosition = state.Player.GetComponent<PositionComponent>().EncounterPosition;
-    EncounterZone closestZone = state.ClosestZone(playerPosition.X, playerPosition.Y);
-    var youAreHereLabel = this.GetNode<RichTextLabel>("SidebarContainer/YouAreHereLabel");
-    if (closestZone.Contains(playerPosition)) {
-      youAreHereLabel.BbcodeText = string.Format("You are in [b]Sector {0}[/b], [b]{1}[/b]", state.DungeonLevel, closestZone.ZoneName);
-    } else {
-      youAreHereLabel.BbcodeText = string.Format("You are in [b]Sector {0}[/b], near [b]{1}[/b]", state.DungeonLevel, closestZone.ZoneName);
+    public void PrepMenu(EncounterState state) {
+      bool hasIntel = state.Player.GetComponent<PlayerComponent>().KnowsIntel(state.DungeonLevel);
+      ResetZones(state.Zones, state.MapWidth, state.MapHeight, hasIntel);
+
+      // TODO: Add You Are Here onto the starmap too!
+      // You Are Here label
+      var playerPosition = state.Player.GetComponent<PositionComponent>().EncounterPosition;
+      EncounterZone closestZone = state.ClosestZone(playerPosition.X, playerPosition.Y);
+      var youAreHereLabel = GetNode<RichTextLabel>("SidebarContainer/YouAreHereLabel");
+      if (closestZone.Contains(playerPosition)) {
+        youAreHereLabel.BbcodeText = string.Format("You are in [b]Sector {0}[/b], [b]{1}[/b]", state.DungeonLevel, closestZone.ZoneName);
+      } else {
+        youAreHereLabel.BbcodeText = string.Format("You are in [b]Sector {0}[/b], near [b]{1}[/b]", state.DungeonLevel, closestZone.ZoneName);
+      }
     }
-  }
 
-  private void OnTreeEntered() {
-    _closeButton.GrabFocus();
-  }
-
-  public override void _UnhandledKeyInput(InputEventKey @event) {
-    if (@event.IsActionPressed("ui_cancel", true)) {
-      OnButtonPressed(null);
-      return;
+    private void OnTreeEntered() {
+      _closeButton.GrabFocus();
     }
-  }
 
-  private void OnButtonPressed(string zoneId) {
-    var sceneManager = (SceneManager)GetNode("/root/SceneManager");
-    sceneManager.CloseAutopilotMenu(zoneId);
+    public override void _UnhandledKeyInput(InputEventKey @event) {
+      if (@event.IsActionPressed("ui_cancel", true)) {
+        OnButtonPressed(null);
+        return;
+      }
+    }
+
+    private void OnButtonPressed(string zoneId) {
+      var sceneManager = (SceneManager)GetNode("/root/SceneManager");
+      sceneManager.CloseAutopilotMenu(zoneId);
+    }
   }
 }
--- scenes/encounter/InventoryMenu.cs
+++ scenes/encounter/InventoryMenu.cs
@@ -1,79 +1,79 @@
 using Godot;
-using SpaceDodgeRL.scenes;
 using SpaceDodgeRL.scenes.components;
 using SpaceDodgeRL.scenes.entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class InventoryMenu : VBoxContainer {
-  private static PackedScene _inventoryPrefab = GD.Load<PackedScene>("res://scenes/encounter/InventoryEntry.tscn");
+namespace SpaceDodgeRL.scenes.encounter {
+  public class InventoryMenu : VBoxContainer {
+    private static PackedScene _inventoryPrefab = GD.Load<PackedScene>("res://scenes/encounter/InventoryEntry.tscn");
 
-  private Button _closeButton;
-  private Dictionary<string, InventoryEntry> _displayedIdsToEntries;
+    private Button _closeButton;
+    private Dictionary<string, InventoryEntry> _displayedIdsToEntries;
 
-  public override void _Ready() {
-    _closeButton = this.GetNode<Button>("Columns/CloseButton");
-    _closeButton.Connect("pressed", this, nameof(OnCloseButtonPressed));
+    public override void _Ready() {
+      _closeButton = GetNode<Button>("Columns/CloseButton");
+      _closeButton.Connect("pressed", this, nameof(OnCloseButtonPressed));
 
-    _displayedIdsToEntries = new Dictionary<string, InventoryEntry>();
-  }
-
-  private void DisplaySpace(InventoryComponent inventory) {
-    var spaceLabel = this.GetNode<Label>("Header/HeaderHBox/SpaceHeader");
-    spaceLabel.Text = String.Format("({0}/{1})", inventory.InventoryUsed, inventory.InventorySize);
-  }
-
-  public void PrepMenu(InventoryComponent inventory) {
-    _closeButton.GrabFocus();
-
-    this.DisplaySpace(inventory);
-
-    // Do a dumb full pass on both to diff & add/remove
-    var inventoryIdsToEntities = inventory.StoredItems.ToDictionary(e => e.EntityId, e => e);
-
-    var toRemoveThese = _displayedIdsToEntries.Where(e => !inventoryIdsToEntities.ContainsKey(e.Key)).Select(e => e.Key).ToList();
-    foreach(string toRemove in toRemoveThese) {
-      _displayedIdsToEntries[toRemove].QueueFree();
-      _displayedIdsToEntries.Remove(toRemove);
+      _displayedIdsToEntries = new Dictionary<string, InventoryEntry>();
     }
 
-    var toAddThese = inventoryIdsToEntities.Where(e => !_displayedIdsToEntries.ContainsKey(e.Key)).ToList();
-    foreach(KeyValuePair<string, Entity> held in toAddThese) {
-      var newEntry = _inventoryPrefab.Instance() as InventoryEntry;
+    private void DisplaySpace(InventoryComponent inventory) {
+      var spaceLabel = GetNode<Label>("Header/HeaderHBox/SpaceHeader");
+      spaceLabel.Text = string.Format("({0}/{1})", inventory.InventoryUsed, inventory.InventorySize);
+    }
 
-      var description = held.Value.GetComponent<DisplayComponent>().Description;
-      newEntry.PopulateData(held.Key, held.Value.EntityName, description);
-      newEntry.Connect(nameof(InventoryEntry.UseItemSelected), this, nameof(OnUseButtonPressed), new Godot.Collections.Array{ held.Key });
+    public void PrepMenu(InventoryComponent inventory) {
+      _closeButton.GrabFocus();
 
-      this._displayedIdsToEntries[held.Key] = newEntry;
+      DisplaySpace(inventory);
 
-      // TODO: A less fixed layout
-      // TODO: When you use an item, it should collapse the column instead of leaving a hole - maybe we should just redraw it all?
-      if (this._displayedIdsToEntries.Count < 10) {
-        GetNode<VBoxContainer>("Columns/LeftColumn").AddChild(newEntry);
-      } else if (this._displayedIdsToEntries.Count < 20) {
-        GetNode<VBoxContainer>("Columns/MiddleColumn").AddChild(newEntry);
-      } else {
-        GetNode<VBoxContainer>("Columns/RightColumn").AddChild(newEntry);
+      // Do a dumb full pass on both to diff & add/remove
+      var inventoryIdsToEntities = inventory.StoredItems.ToDictionary(e => e.EntityId, e => e);
+
+      var toRemoveThese = _displayedIdsToEntries.Where(e => !inventoryIdsToEntities.ContainsKey(e.Key)).Select(e => e.Key).ToList();
+      foreach (string toRemove in toRemoveThese) {
+        _displayedIdsToEntries[toRemove].QueueFree();
+        _displayedIdsToEntries.Remove(toRemove);
+      }
+
+      var toAddThese = inventoryIdsToEntities.Where(e => !_displayedIdsToEntries.ContainsKey(e.Key)).ToList();
+      foreach (KeyValuePair<string, Entity> held in toAddThese) {
+        var newEntry = _inventoryPrefab.Instance() as InventoryEntry;
+
+        var description = held.Value.GetComponent<DisplayComponent>().Description;
+        newEntry.PopulateData(held.Key, held.Value.EntityName, description);
+        newEntry.Connect(nameof(InventoryEntry.UseItemSelected), this, nameof(OnUseButtonPressed), new Godot.Collections.Array { held.Key });
+
+        _displayedIdsToEntries[held.Key] = newEntry;
+
+        // TODO: A less fixed layout
+        // TODO: When you use an item, it should collapse the column instead of leaving a hole - maybe we should just redraw it all?
+        if (_displayedIdsToEntries.Count < 10) {
+          GetNode<VBoxContainer>("Columns/LeftColumn").AddChild(newEntry);
+        } else if (_displayedIdsToEntries.Count < 20) {
+          GetNode<VBoxContainer>("Columns/MiddleColumn").AddChild(newEntry);
+        } else {
+          GetNode<VBoxContainer>("Columns/RightColumn").AddChild(newEntry);
+        }
       }
     }
-  }
 
-  public override void _UnhandledKeyInput(InputEventKey @event) {
-    if (@event.IsActionPressed("ui_cancel", true)) {
-      OnCloseButtonPressed();
-      return;
+    public override void _UnhandledKeyInput(InputEventKey @event) {
+      if (@event.IsActionPressed("ui_cancel", true)) {
+        OnCloseButtonPressed();
+        return;
+      }
     }
-  }
 
-  private void OnUseButtonPressed(string entityId) {
-    var sceneManager = (SceneManager)GetNode("/root/SceneManager");
-    sceneManager.HandleItemToUseSelected(entityId);
-  }
+    private void OnUseButtonPressed(string entityId) {
+      var sceneManager = (SceneManager)GetNode("/root/SceneManager");
+      sceneManager.HandleItemToUseSelected(entityId);
+    }
 
-  private void OnCloseButtonPressed() {
-    var sceneManager = (SceneManager)GetNode("/root/SceneManager");
-    sceneManager.ReturnToPreviousScene();
+    private void OnCloseButtonPressed() {
+      var sceneManager = (SceneManager)GetNode("/root/SceneManager");
+      sceneManager.ReturnToPreviousScene();
+    }
   }
 }
