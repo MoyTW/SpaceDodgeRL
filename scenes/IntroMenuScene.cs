61294dac74b56d64d79350586214699c6197678a
MoyTW <moytravis@gmail.com>
2020-12-12 12:03:30 -0800

    Add exit, settings to main menu

--- scenes/IntroMenuScene.cs
+++ scenes/IntroMenuScene.cs
@@ -3,11 +3,21 @@ using Godot;
 public class IntroMenuScene : Control {
   public override void _Ready() {
     this.GetNode<SaveSlotScene>("CenterContainer/SaveSlot1").FocusLoadButton();
+    this.GetNode<Button>("CenterContainer/SettingsButton").Connect("pressed", this, nameof(OnSettingsButtonPressed));
+    this.GetNode<Button>("CenterContainer/ExitButton").Connect("pressed", this, nameof(OnExitButtonPressed));
   }
 
   // TODO: There's a weird bug when you have some empty save files and you navigate left from a clear button
   // TODO: Focus on the file that you just came from
   public void SetFocus() {
     this.GetNode<SaveSlotScene>("CenterContainer/SaveSlot1").FocusLoadButton();
+  }
+
+  private void OnSettingsButtonPressed() {
+    GD.Print("Attempting to access settings.");
+  }
+
+  private void OnExitButtonPressed() {
+    GetTree().Quit();
   }
 }
