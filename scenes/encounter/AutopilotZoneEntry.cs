eaa7623b6912e413260a3a34a58c89f9cf7cff72
MoyTW <moytravis@gmail.com>
2020-12-29 10:47:57 -0800

    Properly namespace entries
    ahhahaha, just kinda a  nitpick WRT editor integration

--- scenes/encounter/AutopilotZoneEntry.cs
+++ scenes/encounter/AutopilotZoneEntry.cs
@@ -1,59 +1,61 @@
 using Godot;
 using SpaceDodgeRL.scenes.encounter.state;
 
-public class AutopilotZoneEntry : Button {
+namespace SpaceDodgeRL.scenes.encounter {
+  public class AutopilotZoneEntry : Button {
 
-  private TextureRect BuildTextureRect(string path, float squareWidth) {
-    var textureRect = new TextureRect();
+    private TextureRect BuildTextureRect(string path, float squareWidth) {
+      var textureRect = new TextureRect();
 
-    var texture = GD.Load<Texture>(path);
-    textureRect.Texture = texture;
-    // This forces the size to be in line with the itemsBar - I don't know of a more elegant way to do this.
-    textureRect.Expand = true;
-    textureRect.RectMinSize = new Vector2(squareWidth, squareWidth);
-    textureRect.StretchMode = TextureRect.StretchModeEnum.KeepAspect;
+      var texture = GD.Load<Texture>(path);
+      textureRect.Texture = texture;
+      // This forces the size to be in line with the itemsBar - I don't know of a more elegant way to do this.
+      textureRect.Expand = true;
+      textureRect.RectMinSize = new Vector2(squareWidth, squareWidth);
+      textureRect.StretchMode = TextureRect.StretchModeEnum.KeepAspect;
 
-    return textureRect;
-  }
-
-  public void SetReadout(EncounterState state, EncounterZone zone, bool hasIntel) {
-    this.GetNode<Label>("ReadoutContainer/NameEncounterBar/ZoneNameLabel").Text = zone.ZoneName;
-    if (hasIntel) {
-      this.GetNode<Label>("ReadoutContainer/NameEncounterBar/ZoneEncounterLabel").Text = zone.ReadoutEncounterName;
-    } else {
-      this.GetNode<Label>("ReadoutContainer/NameEncounterBar/ZoneEncounterLabel").Text = "NO INTEL";
+      return textureRect;
     }
 
-    var itemsBar = this.GetNode<HBoxContainer>("ReadoutContainer/ItemsFeaturesBar/ItemsBar");
-    if (hasIntel) {
-      var itemsTextureSize = itemsBar.RectSize.y;
-      foreach (var itemReadout in zone.ReadoutItems) {
-        if (state.GetEntityById(itemReadout.EntityId) != null) {
-          itemsBar.AddChild(BuildTextureRect(itemReadout.TexturePath, itemsTextureSize));
-        } else {
-          itemsBar.AddChild(BuildTextureRect("res://resources/checkmark_18x18.png", itemsTextureSize));
-        }
+    public void SetReadout(EncounterState state, EncounterZone zone, bool hasIntel) {
+      GetNode<Label>("ReadoutContainer/NameEncounterBar/ZoneNameLabel").Text = zone.ZoneName;
+      if (hasIntel) {
+        GetNode<Label>("ReadoutContainer/NameEncounterBar/ZoneEncounterLabel").Text = zone.ReadoutEncounterName;
+      } else {
+        GetNode<Label>("ReadoutContainer/NameEncounterBar/ZoneEncounterLabel").Text = "NO INTEL";
       }
-    } else {
-      var noIntelLabel = new Label();
-      noIntelLabel.Text = "-?-";
-      itemsBar.AddChild(noIntelLabel);
-    }
 
-    var featuresBar = this.GetNode<HBoxContainer>("ReadoutContainer/ItemsFeaturesBar/FeaturesBar");
-    if (hasIntel) {
-      var featuresTextureSize = featuresBar.RectSize.y;
-      foreach (var featureReadout in zone.ReadoutFeatures) {
-        if (state.GetEntityById(featureReadout.EntityId) != null) {
-          featuresBar.AddChild(BuildTextureRect(featureReadout.TexturePath, featuresTextureSize));
-        } else {
-          featuresBar.AddChild(BuildTextureRect("res://resources/checkmark_18x18.png", featuresTextureSize));
+      var itemsBar = GetNode<HBoxContainer>("ReadoutContainer/ItemsFeaturesBar/ItemsBar");
+      if (hasIntel) {
+        var itemsTextureSize = itemsBar.RectSize.y;
+        foreach (var itemReadout in zone.ReadoutItems) {
+          if (state.GetEntityById(itemReadout.EntityId) != null) {
+            itemsBar.AddChild(BuildTextureRect(itemReadout.TexturePath, itemsTextureSize));
+          } else {
+            itemsBar.AddChild(BuildTextureRect("res://resources/checkmark_18x18.png", itemsTextureSize));
+          }
         }
+      } else {
+        var noIntelLabel = new Label();
+        noIntelLabel.Text = "-?-";
+        itemsBar.AddChild(noIntelLabel);
       }
-    } else {
-      var noIntelLabel = new Label();
-      noIntelLabel.Text = "-?-";
-      featuresBar.AddChild(noIntelLabel);
+
+      var featuresBar = GetNode<HBoxContainer>("ReadoutContainer/ItemsFeaturesBar/FeaturesBar");
+      if (hasIntel) {
+        var featuresTextureSize = featuresBar.RectSize.y;
+        foreach (var featureReadout in zone.ReadoutFeatures) {
+          if (state.GetEntityById(featureReadout.EntityId) != null) {
+            featuresBar.AddChild(BuildTextureRect(featureReadout.TexturePath, featuresTextureSize));
+          } else {
+            featuresBar.AddChild(BuildTextureRect("res://resources/checkmark_18x18.png", featuresTextureSize));
+          }
+        }
+      } else {
+        var noIntelLabel = new Label();
+        noIntelLabel.Text = "-?-";
+        featuresBar.AddChild(noIntelLabel);
+      }
     }
   }
 }
--- scenes/encounter/InventoryEntry.cs
+++ scenes/encounter/InventoryEntry.cs
@@ -1,58 +1,60 @@
 using Godot;
 
-public class InventoryEntry : Button {
-  public string EntityId { get; private set; }
+namespace SpaceDodgeRL.scenes.encounter {
+  public class InventoryEntry : Button {
+    public string EntityId { get; private set; }
 
-  [Signal]
-  public delegate void UseItemSelected();
+    [Signal]
+    public delegate void UseItemSelected();
 
-  private Label _nameLabel;
-  private Label _descriptionLabel;
+    private Label _nameLabel;
+    private Label _descriptionLabel;
 
-  public override void _Ready() {
-    this.Connect("focus_entered", this, nameof(OnFocusEntered));
-    this.Connect("focus_exited", this, nameof(OnFocusExited));
-    this.Connect("mouse_entered", this, nameof(OnMouseEntered));
-    this.Connect("mouse_exited", this, nameof(OnMouseExited));
-    this.Connect("pressed", this, nameof(OnUseButtonPressed));
-  }
-
-  public void SetData(string entityId, string name, string description) {
-    this.EntityId = entityId;
-
-    if (this._nameLabel == null) {
-      this._nameLabel = this.GetNode<Label>("EntryText/EntryName");
-      this._descriptionLabel =this.GetNode<Label>("EntryText/EntryDescription");
+    public override void _Ready() {
+      Connect("focus_entered", this, nameof(OnFocusEntered));
+      Connect("focus_exited", this, nameof(OnFocusExited));
+      Connect("mouse_entered", this, nameof(OnMouseEntered));
+      Connect("mouse_exited", this, nameof(OnMouseExited));
+      Connect("pressed", this, nameof(OnUseButtonPressed));
     }
 
-    this._nameLabel.Text = name;
-    this._descriptionLabel.Text = description;
-    this.OnFocusExited();
-  }
+    public void SetData(string entityId, string name, string description) {
+      EntityId = entityId;
 
-  // Because we're painting labels on top of a button, the font doesn't switch automatically on hover, so we must do it here.
-  private void OnFocusEntered() {
-    Color backgroundColor = (Color)this.Theme.GetStylebox("normal", "Button").Get("bg_color");
-    this._nameLabel.AddColorOverride("font_color", backgroundColor);
-    this._descriptionLabel.AddColorOverride("font_color", backgroundColor);
-  }
+      if (_nameLabel == null) {
+        _nameLabel = GetNode<Label>("EntryText/EntryName");
+        _descriptionLabel = GetNode<Label>("EntryText/EntryDescription");
+      }
 
-  private void OnFocusExited() {
-    Color normalColor = (Color)this.Theme.GetColor("font_color", "Button");
-    this._nameLabel.AddColorOverride("font_color", normalColor);
-    this._descriptionLabel.AddColorOverride("font_color", normalColor);
-  }
+      _nameLabel.Text = name;
+      _descriptionLabel.Text = description;
+      OnFocusExited();
+    }
 
-  // Focus != hover, but we'd like if it were - hence we synchronize the mouse to keyboard.
-  private void OnMouseEntered() {
-    this.GrabFocus();
-  }
+    // Because we're painting labels on top of a button, the font doesn't switch automatically on hover, so we must do it here.
+    private void OnFocusEntered() {
+      Color backgroundColor = (Color)Theme.GetStylebox("normal", "Button").Get("bg_color");
+      _nameLabel.AddColorOverride("font_color", backgroundColor);
+      _descriptionLabel.AddColorOverride("font_color", backgroundColor);
+    }
 
-  private void OnMouseExited() {
-    this.ReleaseFocus();
-  }
+    private void OnFocusExited() {
+      Color normalColor = Theme.GetColor("font_color", "Button");
+      _nameLabel.AddColorOverride("font_color", normalColor);
+      _descriptionLabel.AddColorOverride("font_color", normalColor);
+    }
 
-  private void OnUseButtonPressed() {
-    EmitSignal(nameof(UseItemSelected));
+    // Focus != hover, but we'd like if it were - hence we synchronize the mouse to keyboard.
+    private void OnMouseEntered() {
+      GrabFocus();
+    }
+
+    private void OnMouseExited() {
+      ReleaseFocus();
+    }
+
+    private void OnUseButtonPressed() {
+      EmitSignal(nameof(UseItemSelected));
+    }
   }
 }
