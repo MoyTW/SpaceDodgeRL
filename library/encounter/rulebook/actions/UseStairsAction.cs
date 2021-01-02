667f0574ef791a88996c2721700007287d191763
MoyTW <moytravis@gmail.com>
2021-01-01 16:40:23 -0800

    Unnecessary TODO idea; remove

--- library/encounter/rulebook/actions/UseStairsAction.cs
+++ library/encounter/rulebook/actions/UseStairsAction.cs
@@ -1,7 +1,5 @@
 namespace SpaceDodgeRL.library.encounter.rulebook.actions {
 
-  // TODO: Wrap this into the generic "use" action? as in, "use stairs" command could just map to "find, and then 'use' the
-  // Usable stairs Entity on the square"
   public class UseStairsAction : EncounterAction {
     public UseStairsAction(string actorId) : base(actorId, ActionType.USE_STAIRS) { }
   }
